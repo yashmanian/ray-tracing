e9a3d3bf2efdcc9bd1a112b993495eb6855629fb
yashmanian <yash.manian@iamrobotics.com>
2019-08-19 21:47:37 -0400

    linear interpolation of color based on rays

--- src/app/rayTest.cpp
+++ src/app/rayTest.cpp
@@ -3,8 +3,14 @@
 //
 #include <iostream>
 #include <fstream>
-#include "Vector3.h"
 #include "Ray.h"
+
+Vector3 color(const Ray& ray)
+{
+    Vector3 unitDirection = unitVector(ray.direction());
+    float t = 0.5f * (unitDirection.y() + 1.0f);
+    return (1.0f - t)*Vector3(1.0f, 1.0f, 1.0f) + t*Vector3(0.5f, 0.7f, 1.0f);
+}
 
 int main()
 {
@@ -12,15 +18,22 @@ int main()
     int ny = 100;
 
     std::ofstream ofs;
-    ofs.open("../rayTraceTest.ppm");
+    ofs.open("rayTraceTest.ppm");
 
     ofs << "P3\n" << nx << " " << ny << "\n255\n";
+    Vector3 lowerLeftCorner(-2.0f, -1.0f, -1.0f);
+    Vector3 horizontalVector(4.0f, 0.0f, 0.0f);
+    Vector3 verticalVector(0.0f, 2.0f, 0.0f);
+    Vector3 origin(0.0f, 0.0f, 0.0f);
 
     for(int j = ny - 1; j >= 0 ; j--)
     {
         for (int i = 0; i < nx; i++)
         {
-            Vector3 col(float(i)/float(nx), float(j)/float(ny), 0.2);
+            float u = float(i)/float(nx);
+            float v = float(j)/float(ny);
+            Ray r(origin, lowerLeftCorner + u*horizontalVector + v*verticalVector);
+            Vector3 col = color(r);
             int ir = int(255.99*col.x());
             int ig = int(255.99*col.y());
             int ib = int(255.99*col.z());
